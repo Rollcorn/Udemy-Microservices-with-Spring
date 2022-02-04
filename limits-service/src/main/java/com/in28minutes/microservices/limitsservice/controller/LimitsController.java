243272937844d987e950de261d6c001cd32ed5fb
denis shkandyuk <lazydaimon@gmail.com>
2022-02-04 18:38:58 +0300

    Update

---
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,17 @@
+package com.in28minutes.microservices.limitsservice.controller;
+
+
+import com.in28minutes.microservices.limitsservice.limitsservice.bean.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+
+    }
+
+
+}
