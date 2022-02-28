5043b820fd938969a740740a3bc4883b697491d0
rollcorn <lazydaimon@gmail.com>
2022-03-01 02:55:38 +0300

    End with microservice-2. Started and finished docker basics

--- microservice-2/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/currencyexchangecontroller/CurrencyExchangeController.java
+++ microservice-2/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/currencyexchangecontroller/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.in28minutes.microservices.currencyexchangeservice.currencyexchangecontroller;
 
 import com.in28minutes.microservices.currencyexchangeservice.CurrencyExchangeServiceApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+
+    Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
     @Autowired
     private CurrencyExchangeRepository repository;
@@ -23,6 +27,8 @@ public class CurrencyExchangeController {
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to) {
+
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
 
         CurrencyExchange currencyExchange =
                 repository.findByFromAndTo(from, to);
